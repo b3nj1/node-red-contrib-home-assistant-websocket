9576daad0abb26a0c5473748c25ab501e8667f59
Benjamin Dodge <benjamin.dodge@gmail.com>
2020-11-30 12:38:49 -0800

    rebase

--- nodes/events-state-changed/events-state-changed.js
+++ nodes/events-state-changed/events-state-changed.js
@@ -116,7 +116,7 @@ module.exports = function (RED) {
             if (validTimer) {
                 if (
                     // If if state is not used and prev and current state is the same return because  timer should already be running
-                    oldState.state === newState.state ||
+                    oldState && oldState.state === newState.state ||
                     // Don't run timers for on connect updates
                     runAll ||
                     // Timer already active and ifState is still true turn don't update
